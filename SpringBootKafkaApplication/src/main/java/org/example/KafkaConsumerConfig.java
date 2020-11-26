16d12e358c337d881f37b67fbfb35c3592f07472
Apollinary <apollinariiuvarov@mail.ru>
2020-11-26 09:19:09 +0300

    Configs from properties file
    Kafka configs are declared in application.properties

--- SpringBootKafkaApplication/src/main/java/org/example/KafkaConsumerConfig.java
+++ SpringBootKafkaApplication/src/main/java/org/example/KafkaConsumerConfig.java
@@ -1,18 +1,15 @@
 package org.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.kafka.inbound.KafkaMessageDrivenChannelAdapter;
 import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.KafkaMessageListenerContainer;
 import org.springframework.messaging.MessageChannel;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class KafkaConsumerConfig {
@@ -31,23 +28,11 @@ public class KafkaConsumerConfig {
     @Bean
     public KafkaMessageListenerContainer<String, String> container() throws Exception {
         ContainerProperties properties = new ContainerProperties(topicFrom);
-        return new KafkaMessageListenerContainer<>(consumerFactory(), properties);
+        return new KafkaMessageListenerContainer<>(consumerFactory, properties);
     }
 
-    @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
-        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
-    }
-
-    @Bean
-    public Map<String, Object> consumerConfigs() {
-        Map<String, Object> props = new HashMap<>();
-        props.put("bootstrap.servers", "localhost:9092");
-        props.put("group.id", "test");
-        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        return props;
-    }
+    @Autowired
+    ConsumerFactory<String, String> consumerFactory;
 
     @Bean
     public MessageChannel kafkaChannel() {
--- SpringBootKafkaApplication/src/main/java/org/example/KafkaProducerConfig.java
+++ SpringBootKafkaApplication/src/main/java/org/example/KafkaProducerConfig.java
@@ -1,42 +1,27 @@
 package org.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.expression.common.LiteralExpression;
 import org.springframework.integration.kafka.outbound.KafkaProducerMessageHandler;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.messaging.MessageHandler;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class KafkaProducerConfig {
     @Value("${kafka.topicTo}")
     private String topicTo;
 
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
     @Bean
     public MessageHandler messageHandler() throws Exception {
         KafkaProducerMessageHandler<String, String> handler =
-                new KafkaProducerMessageHandler<>(kafkaTemplate());
+                new KafkaProducerMessageHandler<>(kafkaTemplate);
         handler.setTopicExpression(new LiteralExpression(topicTo));
         return handler;
-    }
-
-    @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
-    }
-
-    @Bean
-    public ProducerFactory<String, String> producerFactory() {
-        Map<String, Object> props = new HashMap<>();
-        props.put("bootstrap.servers", "localhost:9092");
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        return new DefaultKafkaProducerFactory<>(props);
     }
 }
